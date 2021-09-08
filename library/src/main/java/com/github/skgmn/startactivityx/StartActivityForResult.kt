6d6f82782cde4bbe39d9fa9177abf421e04d63f5
skgmn <suckgamony@gmail.com>
2021-09-08 09:15:59 +0900

    fix unexpected transition animation on startActivityForResult()

--- library/src/main/java/com/github/skgmn/startactivityx/StartActivityForResult.kt
+++ library/src/main/java/com/github/skgmn/startactivityx/StartActivityForResult.kt
@@ -1,5 +1,6 @@
 package com.github.skgmn.startactivityx
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -8,11 +9,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
 suspend fun Context.startActivityForResult(intent: Intent): ActivityResult {
-    return if (this is FragmentActivity) {
-        startActivityForResult(intent)
-    } else {
-        StartActivityHelperUtils.launchHelperActivity(this)
-            .startActivityForResult(intent)
+    return when (this) {
+        is FragmentActivity -> {
+            startActivityForResult(intent)
+        }
+        is Activity -> {
+            StartActivityHelperUtils.launchHelperActivity(this)
+                .startActivityForResult(intent)
+        }
+        else -> {
+            StartActivityHelperUtils.launchHelperActivity(this)
+                .startActivityForResult(intent)
+        }
     }
 }
 
